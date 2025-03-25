99903b30bb5461356c1ddc314a18569bb3431a06
newveela <newveelabandi316677@gmail.com>
2025-03-25 09:20:59 -0400

    Corsconfig

--- src/main/java/com/newveela/bonafidegenerator/controller/StudentController.java
+++ src/main/java/com/newveela/bonafidegenerator/controller/StudentController.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "https://bonafidegenerator.up.railway.app")
 @RequestMapping("/students")
 public class StudentController {
 
